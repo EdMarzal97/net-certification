a6fd06e322263672ebfc05ea51c76c834fa2edc3
Edward S Marzal <edsanchezmarzal@gmail.com>
2023-09-01 16:24:36 -0500

    more practice

--- Program.cs
+++ Program.cs
@@ -5,9 +5,20 @@ Console.WriteLine(roll); */
 
 // Second part:
 
-Console.WriteLine("a" == "a");
+/*Console.WriteLine("a" == "a");
 Console.WriteLine("a" == "A");
 Console.WriteLine(1 == 2);
 
 string myValue = "a";
 Console.WriteLine(myValue == "a");
+
+string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower()); */
+
+/* Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+Console.WriteLine(myValue != "a"); */

e7ac2caf2517f85d32afa4ef877a384f3aa7d722
Edward S Marzal <edsanchezmarzal@gmail.com>
2023-09-01 16:18:22 -0500

    usando boleanos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿
-Random dice = new Random();
+ /*Random dice = new Random();
 int roll = dice.Next(1,7);
-Console.WriteLine(roll);
+Console.WriteLine(roll); */
+
+// Second part:
+
+Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
